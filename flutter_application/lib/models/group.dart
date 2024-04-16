a86e3bb25b11baf271f844ea8703b94dd0bf4519
Enes Ihsan Celik <enesihsancelik@gmail.com>
2024-04-16 10:12:39 +0200

    Changes to be committed:

--- flutter_application/lib/models/group.dart
+++ flutter_application/lib/models/group.dart
@@ -2,11 +2,13 @@ class Group {
   final int id;
   final String name;
   final String description;
+  final bool isPublic;
 
   const Group({
     required this.id,
     required this.name,
     required this.description,
+    required this.isPublic,
   });
 
   factory Group.fromJson(Map<String, dynamic> json) {
@@ -15,11 +17,13 @@ class Group {
         'id': int id,
         'name': String name,
         'description': String description,
+        'isPublic': bool isPublic,
       } =>
         Group(
           id: id,
           name: name,
           description: description,
+          isPublic: isPublic,
         ),
         _ => throw const FormatException('Failed to build Group.'),
     };
