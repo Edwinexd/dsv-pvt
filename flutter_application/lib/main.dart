bcb51decb85fb430583742fe03f4491041e1b572
oltan53 <oltan53@gmail.com>
2024-04-18 13:33:15 +0200

    profile.page branch

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
