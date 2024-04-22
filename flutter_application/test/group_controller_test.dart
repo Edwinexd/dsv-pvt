55d92ebbdf803fcd30fc1d6469a554bc02d6e1b6
Johan Berg <119810021+Johan-Berg-Engstrand@users.noreply.github.com>
2024-04-22 11:09:32 +0200

    Fixed fetchGroup exception test

--- flutter_application/test/group_controller_test.dart
+++ flutter_application/test/group_controller_test.dart
@@ -52,9 +52,9 @@ void main() {
       int groupId = 1;
       String URL = 'https://jsonplaceholder.typicode.com/albums/1';
 
-      when(client
-        .get(Uri.parse('url'),
-          headers: anyNamed('headers'),))
+      when(client.get(
+        any,
+        headers: anyNamed('headers'),))
         .thenAnswer((_) async =>
           http.Response('Not Found', 404));
       
