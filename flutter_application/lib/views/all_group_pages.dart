1f852cbe60a731de02eb12afc2e6da20c0821ae6
Enes Ihsan Celik <enesihsancelik@gmail.com>
2024-04-24 11:47:25 +0200

    Updated filtering system, minor changes about design.

--- flutter_application/lib/views/all_group_pages.dart
+++ flutter_application/lib/views/all_group_pages.dart
@@ -11,14 +11,11 @@ class AllGroupsPage extends StatefulWidget {
 }
 
 class _AllGroupsPageState extends State<AllGroupsPage> {
-  
-  // List of groups
+  //List of instance groups
   final List<Group> _groups = [
-    const Group(name: 'Lace up!', description: 'blabla', id: 3, isPublic: true),
-    const Group(name: 'DVK Runners', description: 'bla', id: 2, isPublic: false),
+    const Group(id: 3, name: 'Lace up!', description: 'Lace up and lead the way', isPublic: true),
+    const Group(id: 2, name: 'DVK Runners', description: 'Join us!', isPublic: false),
     const Group(id: 4, name: 'Kista Runners', description: 'Best runners!', isPublic: true),
-  
-    //Can add more groups 
   ];
 
   String _searchQuery = '';
@@ -26,52 +23,65 @@ class _AllGroupsPageState extends State<AllGroupsPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Filtered and searched groups
     List<Group> filteredGroups = _groups.where((group) {
       final name = group.name.toLowerCase();
-      return name.contains(_searchQuery.toLowerCase()) &&
-          (_selectedFilter == 'All' /*|| will add filtering condition function here */);
+      final isPublic = group.isPublic;
+
+      bool matchesSearchQuery = name.contains(_searchQuery.toLowerCase());
+      bool isPublicMatch = _selectedFilter == 'All' || (_selectedFilter == 'Public' && isPublic);
+      return matchesSearchQuery && isPublicMatch;
     }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('All Groups'),
+        backgroundColor: Colors.teal[300],
       ),
       body: Column(
         children: [
-          // Search bar
-          Padding(
+          Container(
+            color: Colors.grey[200], // Background color of the box
             padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  _searchQuery = value;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Search',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          // Filter dropdown
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DropdownButtonFormField(
-              value: _selectedFilter,
-              onChanged: (value) {
-                setState(() {
-                  _selectedFilter = value.toString();
-                });
-              },
-              items: const [
-                DropdownMenuItem(value: 'All', child: Text('All')),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _searchQuery = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Search',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8), 
                 
+                DropdownButtonFormField(
+                  value: _selectedFilter,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedFilter = value.toString();
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.teal),
+                    ),
+                  ),
+                  items: const [
+                    DropdownMenuItem(value: 'All', child: Text('All')),
+                    DropdownMenuItem(value: 'Public', child: Text('Public groups')),
+                  ],
+                ),
               ],
             ),
           ),
+          const SizedBox(height: 8),
           
-
           Expanded(
             child: ListView.builder(
               itemCount: filteredGroups.length,
@@ -79,8 +89,13 @@ class _AllGroupsPageState extends State<AllGroupsPage> {
                 final group = filteredGroups[index];
                 return ListTile(
                   title: Text(group.name),
-                  subtitle: Text(group.description),
-                  // Add more details or actions as needed
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(group.description),
+                      Text(group.isPublic ? 'Public' : 'Private'),
+                    ],
+                  ),
                 );
               },
             ),
