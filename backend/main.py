d4fc8b84dbb65831ccc7befe71a907f6ea8b5aa3
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-04-25 11:46:34 +0200

    Add connection to session management service

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 import os
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-import crud, models, schemas
-from database import session_local, engine
-import requests
 from datetime import datetime
+from typing import Annotated
+
+import requests
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.orm import Session
+
+import crud
+import models
+import schemas
+from database import engine, session_local
+from sessions import create_session, get_session
 
 models.base.metadata.create_all(bind = engine)
 
@@ -17,8 +24,19 @@ def get_db_session():
     finally:
         db_session.close()
 
+header_scheme = HTTPBearer()
+
+def get_current_user(token: Annotated[HTTPAuthorizationCredentials, Depends(header_scheme)]) -> schemas.SessionUser:
+    session = get_session(token.credentials)
+    if session is None:
+        raise HTTPException(status_code=401, detail="Unauthorized", headers={"WWW-Authenticate": "Bearer"})
+    
+    return session
+
+
 #USER
 #login
+# TODO: Properly annotate in OPENAPI that it requires credentials
 @app.post("/users/login")
 def login(credentials: schemas.UserCreds):
     try:
@@ -26,7 +44,8 @@ def login(credentials: schemas.UserCreds):
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         raise HTTPException(e.response.status_code, detail=e.response.json()["detail"])
-    return response.json()
+    session = create_session(response.json()["id"])
+    return {"bearer": f"Bearer {session}"}
 
 # user creation
 @app.post("/users", response_model = schemas.User)
@@ -49,27 +68,27 @@ def create_user(user_payload: schemas.UserCreate, db_session: Session = Depends(
 
 # get a list of users from db using a offset and size limit
 @app.get("/users", response_model = schemas.UserList)
-def read_users(skip: int = 0, limit: int = 100, db_session: Session = Depends(get_db_session)):
+def read_users(user: Annotated[schemas.SessionUser, Depends(get_current_user)], skip: int = 0, limit: int = 100, db_session: Session = Depends(get_db_session)):
     users = schemas.UserList(data=crud.get_users(db_session, skip=skip, limit=limit))
     return users
 
 #get a user from db using specific user id
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db_session: Session = Depends(get_db_session)):
+def read_user(user: Annotated[schemas.SessionUser, Depends(get_current_user)], user_id: int, db_session: Session = Depends(get_db_session)):
     db_user = crud.get_user(db_session, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 @app.patch("/users/{user_id}", response_model=schemas.User)
-def update_user(user_id: int, user_update: schemas.UserUpdate, db_session: Session = Depends(get_db_session)):
+def update_user(user: Annotated[schemas.SessionUser, Depends(get_current_user)], user_id: int, user_update: schemas.UserUpdate, db_session: Session = Depends(get_db_session)):
     db_user = crud.get_user(db_session, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return crud.update_user(db_session, db_user, user_update)
 
 @app.delete("/users/{user_id}")
-def delete_user(user_id: int, db_session: Session = Depends(get_db_session)):
+def delete_user(user: Annotated[schemas.SessionUser, Depends(get_current_user)], user_id: int, db_session: Session = Depends(get_db_session)):
     db_user = crud.get_user(db_session, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -78,30 +97,30 @@ def delete_user(user_id: int, db_session: Session = Depends(get_db_session)):
 
 # group creation
 @app.post("/groups", response_model = schemas.Group)
-def create_group(group: schemas.GroupCreate, db_session: Session = Depends(get_db_session)):
+def create_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], group: schemas.GroupCreate, db_session: Session = Depends(get_db_session)):
     return crud.create_group(db_session=db_session, group=group)
 
 @app.get("/groups/{group_id}", response_model=schemas.Group)
-def read_group(group_id: int, db_session: Session = Depends(get_db_session)):
+def read_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], group_id: int, db_session: Session = Depends(get_db_session)):
     db_group = crud.get_group(db_session, group_id=group_id)
     if db_group is None:
         raise HTTPException(status_code=404, detail="Group not found")
     return db_group
 
 @app.get("/groups", response_model=schemas.GroupList)
-def read_groups(skip: int = 0, limit: int = 100, db_session: Session = Depends(get_db_session)):
+def read_groups(user: Annotated[schemas.SessionUser, Depends(get_current_user)], skip: int = 0, limit: int = 100, db_session: Session = Depends(get_db_session)):
     groups = schemas.GroupList(data=crud.get_groups(db_session, skip=skip, limit=limit))
     return groups
 
 @app.patch("/groups/{group_id}", response_model=schemas.Group)
-def update_group(group_id: int, group_update: schemas.GroupUpdate, db_session: Session = Depends(get_db_session)):
+def update_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], group_id: int, group_update: schemas.GroupUpdate, db_session: Session = Depends(get_db_session)):
     db_group = crud.get_group(db_session, group_id=group_id)
     if db_group is None:
         raise HTTPException(status_code=404, detail="Group not found")
     return crud.update_group(db_session, db_group, group_update)
 
 @app.delete("/groups/{group_id}")
-def delete_group(group_id: int, db_session: Session = Depends(get_db_session)):
+def delete_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], group_id: int, db_session: Session = Depends(get_db_session)):
     db_group = crud.get_group(db_session, group_id=group_id)
     if db_group is None:
         raise HTTPException(status_code=404, detail="Group not found")
@@ -111,7 +130,7 @@ def delete_group(group_id: int, db_session: Session = Depends(get_db_session)):
 #MEMBERSHIPS
 # join group
 @app.put("/groups/{group_id}/members/{user_id}", response_model=schemas.Group)
-def join_group(user_id: int, group_id: int, db_session: Session = Depends(get_db_session)):
+def join_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], user_id: int, group_id: int, db_session: Session = Depends(get_db_session)):
     db_user = read_user(user_id=user_id, db_session=db_session)
     db_group = read_group(group_id=group_id, db_session=db_session)
     if db_user in db_group.users:
@@ -120,7 +139,7 @@ def join_group(user_id: int, group_id: int, db_session: Session = Depends(get_db
 
 #leave group
 @app.delete("/groups/{group_id}/members/{user_id}", response_model=schemas.Group)
-def leave_group(user_id: int, group_id: int, db_session: Session = Depends(get_db_session)):
+def leave_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], user_id: int, group_id: int, db_session: Session = Depends(get_db_session)):
     db_user = read_user(user_id=user_id, db_session=db_session)
     db_group = read_group(group_id=group_id, db_session=db_session)
     if db_user not in db_group.users:
@@ -129,13 +148,13 @@ def leave_group(user_id: int, group_id: int, db_session: Session = Depends(get_d
 
 # get all members in a group by group_id
 @app.get("/groups/{group_id}/members", response_model = schemas.UserList)
-def read_members_in_group(group_id: int, db_session: Session = Depends(get_db_session)):
+def read_members_in_group(user: Annotated[schemas.SessionUser, Depends(get_current_user)], group_id: int, db_session: Session = Depends(get_db_session)):
     users = schemas.UserList(data=crud.get_group_users(db_session=db_session, group_id=group_id))
     return users
 
 # get all groups a user has joined
 @app.get("/users/{user_id}/groups", response_model = schemas.GroupList)
-def read_user_groups(user_id: int, db_session: Session = Depends(get_db_session)):
+def read_user_groups(user: Annotated[schemas.SessionUser, Depends(get_current_user)], user_id: int, db_session: Session = Depends(get_db_session)):
     groups = schemas.GroupList(data=crud.get_user_groups(db_session=db_session, user_id=user_id))
     return groups
 
