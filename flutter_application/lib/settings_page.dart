ede67a41e9a239f4c2535c705c8e7dd6d7f9e68f
Nooorab <noorabohasan89@gmail.com>
2024-04-27 12:40:57 +0200

    This has to resolve the conflict in the main with the home-page + fixing a minor bug.

--- flutter_application/lib/settings_page.dart
+++ flutter_application/lib/settings_page.dart
@@ -1,102 +1,113 @@
 import 'package:flutter/material.dart';
 
+
 class SettingsPage extends StatefulWidget {
-  final ValueChanged<bool> onToggleDarkMode;
+ final ValueChanged<bool> onToggleDarkMode;
+ final bool initialDarkMode;
 
-  const SettingsPage({
-    Key? key,
-    required this.onToggleDarkMode,
-  }) : super(key: key);
 
-  @override
-  _SettingsPageState createState() => _SettingsPageState();
+
+
+ const SettingsPage({
+   Key? key,
+   required this.onToggleDarkMode,
+   required this.initialDarkMode,
+ }) : super(key: key);
+
+
+ @override
+ _SettingsPageState createState() => _SettingsPageState();
 }
+
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool darkModeEnabled = false;
+ bool darkModeEnabled = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: const Text('Dark Mode'),
-            trailing: Switch(
-              value: darkModeEnabled,
-              onChanged: (value) {
-                setState(() {
-                  darkModeEnabled = value;
-                  widget.onToggleDarkMode(value);
-              });
-              widget.onToggleDarkMode(value);
-              },
-          ),
-          ),  
-          ListTile(
-            title: const Text('Clear Cache'),
-            onTap: () {
-              // Implement clear cache functionality
-            },
-          ),
-          ListTile(
-            title: const Text('Notification Settings'),
-            onTap: () {
-              // Navigate to notification settings page
-            },
-          ),          
-          ListTile(
-            title: const Text('About App'),
-            onTap: () {
-              // Navigate to About App page
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AboutAppPage(),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text('Settings'),
+     ),
+     body: ListView(
+       children: [
+         ListTile(
+           title: const Text('Dark Mode'),
+           trailing: Switch(
+             value: darkModeEnabled,
+             onChanged: (value) {
+               setState(() {
+                 darkModeEnabled = value;
+                 widget.onToggleDarkMode(value);
+             });
+             widget.onToggleDarkMode(value);
+             },
+         ),
+         ), 
+         ListTile(
+           title: const Text('Clear Cache'),
+           onTap: () {
+             // Implement clear cache functionality
+           },
+         ),
+         ListTile(
+           title: const Text('Notification Settings'),
+           onTap: () {
+             // Navigate to notification settings page
+           },
+         ),         
+         ListTile(
+           title: const Text('About App'),
+           onTap: () {
+             // Navigate to About App page
+             Navigator.push(
+               context,
+               MaterialPageRoute(
+                 builder: (context) => const AboutAppPage(),
+               ),
+             );
+           },
+         ),
+       ],
+     ),
+   );
+ }
 }
 
-class AboutAppPage extends StatelessWidget {
-  const AboutAppPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('About App'),
-      ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Middnatsloppet App',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'This app is designed for Middnatsloppet, a popular event for running enthusiasts. '
-              'With this app, users can track their training progress, participate in events, '
-              'connect with other runners, and more.',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+class AboutAppPage extends StatelessWidget {
+ const AboutAppPage({super.key});
+
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text('About App'),
+     ),
+     body: const SingleChildScrollView(
+       padding: EdgeInsets.all(16.0),
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Text(
+             'Middnatsloppet App',
+             style: TextStyle(
+               fontSize: 24,
+               fontWeight: FontWeight.bold,
+             ),
+           ),
+           SizedBox(height: 16),
+           Text(
+             'This app is designed for Middnatsloppet, a popular event for running enthusiasts. '
+             'With this app, users can track their training progress, participate in events, '
+             'connect with other runners, and more.',
+             style: TextStyle(fontSize: 18),
+           ),
+         ],
+       ),
+     ),
+   );
+ }
 }
