922a1a28118a502884225ce2a131433631ca0646
oltan53 <oltan53@gmail.com>
2024-05-01 00:24:47 +1000

    small changes

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const LoginPage(),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
