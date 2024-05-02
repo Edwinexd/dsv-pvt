9d64d00ab996c2f42b12be2599a42e58546a6f6a
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-05-02 11:57:32 +0200

    Get openstreetmap to work Note had to nuke everything else for the time being

--- flutter_application/lib/activity_create.dart
+++ flutter_application/lib/activity_create.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
+import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
 
 //test Mac
 
@@ -60,7 +61,8 @@ class _ActivityCreatePageState extends State<ActivityCreatePage> {
     final TimeOfDay? picked = await showTimePicker(
       context: context,
       initialTime: _pickedTime,
-      builder: (BuildContext context, Widget? child) { // Force 24-hour format
+      builder: (BuildContext context, Widget? child) {
+        // Force 24-hour format
         return MediaQuery(
           data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
           child: child!,
@@ -79,76 +81,99 @@ class _ActivityCreatePageState extends State<ActivityCreatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Create Activity')),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            // TODO: Open street map
-            TextFormField(
-              controller: _titleController,
-              decoration: InputDecoration(labelText: 'Activity Title'),
-              validator: (value) =>
-                  value!.isEmpty ? 'Title cannot be empty' : null,
-            ),
-
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                      'Date & Time: ${DateFormat('EEEE dd MMMM hh:mm').format(_pickedDateTime.toLocal())}', // Use DateFormat to format the date
-                      style: TextStyle(fontSize: 16)),
-                  Column(
-                    // TODO: Frontend people help me place these buttons nicely! / Edwin
-                    children: [
-                      ElevatedButton(
-                        onPressed: _pickDate,
-                        child: Text('Select Date'),
-                      ),
-                      ElevatedButton(
-                        onPressed: _pickTime,
-                        child: Text('Select Time'),
+        appBar: AppBar(title: Text('Create Activity')),
+        body: /*ListView(padding: EdgeInsets.all(16.0), children: <Widget>[*/
+          FlutterLocationPicker(
+            initZoom: 11,
+            minZoomLevel: 5,
+            maxZoomLevel: 16,
+            trackMyPosition: true,
+            searchBarBackgroundColor: Colors.white,
+            selectedLocationButtonTextstyle: const TextStyle(fontSize: 18),
+            mapLanguage: 'en',
+            onError: (e) => print(e),
+            selectLocationButtonLeadingIcon: const Icon(Icons.check),
+            onPicked: (pickedData) {
+              print(pickedData.latLong.latitude);
+              print(pickedData.latLong.longitude);
+              print(pickedData.address);
+              print(pickedData.addressData);
+            },
+            onChanged: (pickedData) {
+              print(pickedData.latLong.latitude);
+              print(pickedData.latLong.longitude);
+              print(pickedData.address);
+              print(pickedData.addressData);
+            },
+            showContributorBadgeForOSM: true,
+          ));/*,
+          Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: <Widget>[
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(labelText: 'Activity Title'),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Title cannot be empty' : null,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                          'Date & Time: ${DateFormat('EEEE dd MMMM hh:mm').format(_pickedDateTime.toLocal())}', // Use DateFormat to format the date
+                          style: TextStyle(fontSize: 16)),
+                      Column(
+                        // TODO: Frontend people help me place these buttons nicely! / Edwin
+                        children: [
+                          ElevatedButton(
+                            onPressed: _pickDate,
+                            child: Text('Select Date'),
+                          ),
+                          ElevatedButton(
+                            onPressed: _pickTime,
+                            child: Text('Select Time'),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Skill Level:', style: TextStyle(fontSize: 16)),
-                  Slider(
-                    value: _difficultyCode.toDouble(),
-                    min: 0,
-                    max: 4,
-                    divisions: 4,
-                    onChanged: (double value) {
-                      setState(() {
-                        _difficultyCode = value.toInt();
-                      });
-                    },
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Skill Level:', style: TextStyle(fontSize: 16)),
+                      Slider(
+                        value: _difficultyCode.toDouble(),
+                        min: 0,
+                        max: 4,
+                        divisions: 4,
+                        onChanged: (double value) {
+                          setState(() {
+                            _difficultyCode = value.toInt();
+                          });
+                        },
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text('${skillLevels[_difficultyCode]}',
+                            style: TextStyle(fontSize: 12)),
+                      )
+                    ],
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: Text('${skillLevels[_difficultyCode]}',
-                        style: TextStyle(fontSize: 12)),
-                  )
-                ],
-              ),
+                ),
+                ElevatedButton(
+                  onPressed: _createActivity,
+                  child: Text('Create Activity'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: _createActivity,
-              child: Text('Create Activity'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ]));*/
   }
 }
