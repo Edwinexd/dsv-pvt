c4fa922d2b34f396c4419fd15f1d2cb37b2cb38b
Axel Andersson <oltan53@gmail.com>
2024-05-07 10:29:53 +0200

    Login signup figma update (#46)
    * branch samt bakgrund
    
    * background sign-up
    
    * Edited login and signup from figma
    
    * back to home: MainPage
    
    * should fixed in be this branch
    
    * comments gone (I hope)

--- flutter_application/lib/views/login_page.dart
+++ flutter_application/lib/views/login_page.dart
@@ -35,156 +35,121 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SafeArea(
-            child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              //logo
-              Icon(
-                Icons.person,
-                size: 100,
-                color: Colors.grey[800],
-              ),
-
-              const SizedBox(height: 40),
-
-              //welcome back, youve been missed
-              Text('Welcome back!',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  )),
-
-              const SizedBox(height: 25),
-
-              // username textfield
-              MyTextField(
-                controller: usernameController,
-                hintText: 'Email',
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 10),
-
-              //password textfield
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true,
-              ),
-
-              const SizedBox(height: 10),
-
-              //forgot password?
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600]),
-                    )
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              //sign in button
-              MyButton(
-                buttonText: 'Sign In',
-                onTap: signUserIn,
-              ),
-
-              const SizedBox(height: 50),
-
-              //or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(
-                height: 50,
-              ),
-
-              //google + apple sign in
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //google button
-                  SquareTile(imagePath: 'lib/images/google.png'),
-
-                  //const SizedBox(width: 25),
-
-                  //apple button
-                  //SquareTile(imagePath: 'lib/images/apple.png'),
-                ],
-              ),
-
-              const SizedBox(height: 50),
-
-              //Not a member? register now
-              // Not a member? Register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Navigate to the SignUpPage
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()),
-                      );
-                    },
-                    child: const Text(
-                      'Register Now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        //decoration: isHovering ? TextDecoration.underline : TextDecoration.none,
-                      ),
-                    ),
-                  ),
-                ],
-              )
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 171, 171, 252),
+              Color.fromARGB(255, 180, 125, 252),
+              Color.fromARGB(255, 210, 120, 252),
+              Color.fromARGB(200, 238, 172, 252),
+              Color.fromARGB(255, 253, 253, 255)
             ],
           ),
-        )));
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                const Icon(
+                  Icons.person,
+                  size: 100,
+                  color: Color.fromARGB(255, 16,14,99),
+                ),
+                const SizedBox(height: 40),
+                const Text('Welcome!',
+                    style: TextStyle(color: Color.fromARGB(255, 16,14,99), fontSize: 16)),
+                const SizedBox(height: 25),
+                MyTextField(
+                  controller: usernameController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Forgot Password?',
+                          style: TextStyle(color: Color.fromARGB(255, 16,14,99)))
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                MyButton(
+                  buttonText: 'Sign In',
+                  onTap: signUserIn,
+                ),
+                const SizedBox(height: 50),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Color.fromARGB(255, 16,14,99),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text('Or continue with',
+                            style: TextStyle(color: Color.fromARGB(255, 16,14,99))),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Color.fromARGB(255, 16,14,99),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquareTile(imagePath: 'lib/images/google.png'),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Not a member?',
+                        style: TextStyle(color: Color.fromARGB(255, 16,14,99))),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpPage()));
+                      },
+                      child: const Text(
+                        'Register Now',
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
