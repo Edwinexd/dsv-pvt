eab2a47db87930c273508a20afa4836dcecab718
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-05-08 12:56:58 +0200

    Run black & fix workflow

--- authentication/passwords.py
+++ authentication/passwords.py
@@ -1,14 +1,18 @@
 import secrets
 import hashlib
 
+
 def generate_salt():
     return secrets.token_urlsafe(256)
+
 
 def _pbkdf2_hmac(password: str, salt: str):
     return hashlib.pbkdf2_hmac("sha256", password.encode(), salt.encode(), 100000).hex()
 
+
 def validate(password: str, salt: str, password_hash: str) -> bool:
     return _pbkdf2_hmac(password, salt) == password_hash
+
 
 def create_password_hash(password: str, salt: str):
     return _pbkdf2_hmac(password, salt)
