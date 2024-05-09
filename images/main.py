29f1c515c362c74372d132d814ce98e993cd56dc
Alfred Berggren <alfred.berggren@gmail.com>
2024-05-09 08:57:04 +0200

    download endpoint seems to work

--- images/main.py
+++ images/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
 from tempfile import NamedTemporaryFile
 import boto3
 from botocore.config import Config
@@ -12,21 +13,33 @@ app = FastAPI()
 endpoint_url = os.getenv("AWS_ENDPOINT_URL")
 api_key_id = os.getenv("API_KEY_ID")
 api_key = os.getenv("API_KEY")
+bucket_name = os.getenv("BUCKET_NAME")
 
-s3 = boto3.resource(service_name='s3', endpoint_url=endpoint_url, aws_access_key_id=api_key_id, aws_secret_access_key=api_key, config = Config(signature_version='s3v4',))
+s3 = boto3.resource(
+    service_name="s3",
+    endpoint_url=endpoint_url,
+    aws_access_key_id=api_key_id,
+    aws_secret_access_key=api_key,
+    config=Config(
+        signature_version="s3v4",
+    ),
+)
 
 ALLOWED_FILE_TYPES = ["image/png", "image/jpeg"]
 
 MAX_FILE_SIZE = 2_097_152
 
-#s3.Bucket("dsv-pvt-images-service").upload_file("/home/alfred/Pictures/Screenshots/testimage.png", "test.png")
-#s3.Bucket("dsv-pvt-images-service").download_file("test.png","/home/alfred/testB2.png")
+# s3.Bucket("dsv-pvt-images-service").upload_file("/home/alfred/Pictures/Screenshots/testimage.png", "test.png")
+# s3.Bucket("dsv-pvt-images-service").download_file("test.png","/home/alfred/testB2.png")
+
+# TODO: better error handling for both of these
+
 
 @app.post("/upload")
-async def upload_image(image: UploadFile):
+async def upload_image(image: UploadFile, dir: str):
     if image.content_type not in ALLOWED_FILE_TYPES:
         raise HTTPException(status_code=400, detail="Bad file type")
-    
+
     if image.size > MAX_FILE_SIZE:
         raise HTTPException(status_code=400, detail="Bad file size")
 
@@ -38,16 +51,24 @@ async def upload_image(image: UploadFile):
 
     image.file.close()
 
-    #os.remove(temp.name)
-    
     try:
-        #bucket.upload_bytes(image.file.read(), image.filename)
-        s3.Bucket("dsv-pvt-images-service").upload_file(temp.name, image.filename)
-        return {"message" : f"image uploaded successfully, file name={image.filename}"}
+        s3.Bucket(bucket_name).upload_file(temp.name, f"{dir}{image.filename}")
+        return {"message": f"image uploaded successfully, file name={image.filename}"}
     except Exception as e:
-        return {"message" : f"error uploading image: {e}"}
-    
-    
+        return {"message": f"error uploading image: {e}"}
+    finally:
+        os.remove(temp.name)
+
+
 @app.post("/download")
 async def download_image(file_name: str):
-    print(file_name)
+    try:
+        with NamedTemporaryFile(delete=False, mode="w+b") as f:
+            s3.Bucket(bucket_name).download_file(file_name, f.name)
+            return FileResponse(
+                path=f.name,
+                filename=file_name,
+                media_type=f"image/{file_name.split('.')[1]}",
+            )
+    except Exception as e:
+        return {"message": f"error downloading image: {e}"}
