cba9ccd9528c4958b7201ecdd0859f47b8a59a64
oltan53 <oltan53@gmail.com>
2024-05-09 12:54:38 +0200

    rewritten basic layout of edit page. rewritten basically, and big changes incoming

--- flutter_application/lib/edit_profile_page.dart
+++ flutter_application/lib/edit_profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/components/my_button.dart';
 import 'package:flutter_application/components/skill_level_slider.dart';
-import 'package:flutter_application/background_for_pages.dart'; // Ensure this import is here
+import 'package:flutter_application/background_for_pages.dart';
 
 class EditProfilePage extends StatefulWidget {
   @override
@@ -9,25 +10,23 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   final _formKey = GlobalKey<FormState>();
-  final _nameController = TextEditingController();
-  final _emailController = TextEditingController();
-  final _interestsController = TextEditingController();
   int _skillLevel = 0;
-
-  String imageUrl = 'https://example.com/new_profile.jpg'; 
+  String imageUrl = 'https://example.com/profile_placeholder.png';
+  bool signedUpToMidnattsloppet = false;
+  Map<String, bool> interests = {
+    'Running': false,
+    'Yoga': false,
+    'Walking': false,
+    'Swimming': false,
+    'Workout': false,
+    'Cycling': false,
+  };
 
   void _saveProfile() {
     if (_formKey.currentState!.validate()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Profile Saved!'))
-      );
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Profile Saved!')));
     }
-  }
-
-  void _pickImage() {
-    setState(() {
-      // Placeholder image change functionality
-    });
   }
 
   @override
@@ -36,75 +35,90 @@ class _EditProfilePageState extends State<EditProfilePage> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
-          title: const Text('Edit Profile', style: TextStyle(color: Colors.white),),
+          title: Text('Edit Profile'),
           backgroundColor: Colors.transparent,
           elevation: 0,
-          iconTheme: const IconThemeData(color: Colors.white,),
         ),
         body: Form(
           key: _formKey,
-          child: ListView(
-            padding: EdgeInsets.all(16.0),
-            children: <Widget>[
-              Center(
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 70,
-                      backgroundImage: NetworkImage(imageUrl),
-                    ),
-                    Positioned(
-                      right: -10,
-                      top: 95,
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Username',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold)),
+                        IconButton(
+                          icon: Icon(Icons.edit, color: Theme.of(context).primaryColor),
+                          onPressed: () {
+                            // Implement the functionality to edit the username
+                          },
                         ),
-                        child: IconButton(
-                          icon: const Icon(Icons.camera_alt, color: Color.fromARGB(255, 255, 92, 00)),
-                          onPressed: _pickImage,
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 70.0,
+                        backgroundImage: NetworkImage(imageUrl),
+                      ),
+                      Positioned(
+                        right: -10,
+                        top: 95,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                spreadRadius: 1,
+                                blurRadius: 3,
+                                offset: const Offset(0, 1),
+                              ),
+                            ],
+                          ),
+                          child: IconButton(
+                              icon: const Icon(Icons.edit,
+                                  color: Color.fromARGB(255, 255, 92, 00)),
+                              onPressed: () {
+                                // Implement functionality to edit profile image
+                              }),
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Bio'),
+                  ),
+                  SkillLevelSlider(
+                    initialSkillLevel: _skillLevel,
+                    onSkillLevelChanged: (newLevel) {
+                      setState(() {
+                        _skillLevel = newLevel;
+                      });
+                    },
+                  ),
+                  MyButton(
+                    buttonText: 'Save Profile',
+                    onTap: _saveProfile,
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: 'Name'),
-                validator: (value) => value!.isEmpty ? 'Name cannot be empty' : null,
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                validator: (value) => !value!.contains('@') ? 'Please enter a valid email' : null,
-              ),
-              TextFormField(
-                controller: _interestsController,
-                decoration: const InputDecoration(labelText: 'Interests'),
-                validator: (value) => value!.isEmpty ? 'Interests cannot be empty' : null,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: SkillLevelSlider(
-                  initialSkillLevel: _skillLevel,
-                  onSkillLevelChanged: (int newLevel) {
-                    setState(() {
-                      _skillLevel = newLevel;
-                    });
-                  },
-                ),
-              ),
-              ElevatedButton(
-                onPressed: _saveProfile,
-                child: const Text('Save Profile'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
