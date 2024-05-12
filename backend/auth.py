7b60fb38a2e2eefd17bd9a9cded8a3c3cb23e271
theaekmark <thea.ekmark@gmail.com>
2024-05-13 00:07:42 +0200

    client_id and so on

--- backend/auth.py
+++ backend/auth.py
@@ -2,6 +2,9 @@ import os
 import requests
 from schemas import UserCreate, UserCreds
 from fastapi import HTTPException
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 
 AUTH_URL = os.getenv("AUTH_URL")
 
@@ -20,3 +23,43 @@ def create_user(user_payload: UserCreate):
     if not response.ok:
         raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
     return str(response.json()["id"])
+
+
+REDIRECT_URI = 'http://localhost:5000/callback'
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+def exchange_code_for_token(code):
+    token_url = 'https://oauth2.googleapis.com/token'
+    data = {
+        'code' : code,
+        'client_id' : CLIENT_ID,
+        'client_secret' : CLIENT_SECRET,
+        'redirect_uri' : REDIRECT_URI,
+        'grant_type' : 'authorization_code'
+    }
+    response = requests.post(token_url, data=data)
+    return response.json().get('access_token')
+
+
+# (Receive token by HTTPS POST)
+def authenticate(access_token):
+    try:
+    # Specify the CLIENT_ID of the app that accesses the backend:
+        idinfo = id_token.verify_oauth2_token(access_token, requests.Request(), CLIENT_ID)
+
+    # Or, if multiple clients access the backend server:
+    # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Invalid issuer')
+        return idinfo 
+
+    # If the request specified a Google Workspace domain
+    # if idinfo['hd'] != DOMAIN_NAME:
+    #     raise ValueError('Wrong domain name.')
+
+    # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+    except ValueError:
+    # Invalid token
+        pass
--- backend/oauth.py
+++ backend/oauth.py
@@ -1,26 +1,19 @@
+from auth import exchange_code_for_token, authenticate
 
-from google.oauth2 import id_token
-from google.auth.transport import requests
+src="https://apis.google.com/js/platform.js"
+#<script src="https://apis.google.com/js/platform.js" async defer></script>
 
+#google sign in button:
+#<div class="g-signin2" data-onsuccess="onSignIn"></div>
 
 #Scope = https://www.googleapis.com/auth/userinfo.email
-
-REDIRECT_URI = 'http://localhost:5000/callback'
-
-
-
-
-
-
-
-# (Receive token by HTTPS POST)
-try:
+    #try:
     # Specify the CLIENT_ID of the app that accesses the backend:
-    idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+      #  idinfo = id_token.verify_oauth2_token(access_token, requests.Request(), CLIENT_ID)
 
     # Or, if multiple clients access the backend server:
     # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-    # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+    #if idinfo['iss'] not in ['account', CLIENT_ID_2, CLIENT_ID_3]:
     #     raise ValueError('Could not verify audience.')
 
     # If the request specified a Google Workspace domain
@@ -28,7 +21,42 @@ try:
     #     raise ValueError('Wrong domain name.')
 
     # ID token is valid. Get the user's Google Account ID from the decoded token.
-    userid = idinfo['sub']
-except ValueError:
+      #  userid = idinfo['sub']
+    #except ValueError:
     # Invalid token
-    pass
+     #   pass
+
+# (Receive token by HTTPS POST)
+
+
+    #SIGN IN
+
+    #SIGN OUT
+
+from auth import exchange_code_for_token, authenticate
+
+class AuthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_url = urlparse(self.path)
+        params = parse_qs(parsed_url.query)
+
+        if parsed_url.path == '/login':
+            auth_url = f"https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=email"
+            self.send_response(302)
+            self.send_header('Location', auth_url)
+            self.end_headers()
+
+        elif parsed_url.path == '/callback':
+            code = params.get('code', [None])[0]
+            if code:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                #self.wfile.write(b'<html><body>Successfully authenticated! You can close this window now.</body></html>')
+
+                access_token = exchange_code_for_token(code)
+                user_info = authenticate(access_token)
+                if user_info:
+                    print('User Info:', user_info)
+                else:
+                    print('Authentication failed.')
