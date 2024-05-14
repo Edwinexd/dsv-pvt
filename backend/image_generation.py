6a6a35de20cf7c80ca3e39b56dad68ee1582e359
Alfred Berggren <alfred.berggren@gmail.com>
2024-05-15 00:14:03 +0200

    add rounded corners

--- backend/image_generation.py
+++ backend/image_generation.py
@@ -7,69 +7,106 @@ from pydantic import BaseModel
 import io
 from dataclasses import dataclass
 
+
 @dataclass
-class SubGradient():
-    percentage: int
+class SubGradient:
+    percentage: int = 0
     r: int = 0xFF
     g: int = 0xFF
     b: int = 0xFF
-    opacity: float
+    opacity: float = 1
 
+# TODO: add drop shadow
 def generate_image(**data):
     # TODO: assert some sort of standard for kwargs e.g.
     # {title: "foo", user: "bar", image_id: "sna"...}
 
-    s = Image.new(mode="RGB", size=(1024,1024))
-    base = Image.new(mode="RGB", size=(1024,1024))
+    #s = Image.new(mode="RGBA", size=(1024, 1024))
+    base = Image.new(mode="RGBA", size=(1024, 1024))
+    base.paste((255, 255, 255), (0, 0, base.width, base.height))
     draw_gradient(
         base,
         [
             SubGradient(percentage=23, r=0xAB, g=0xAB, b=0xFC, opacity=0.9),
             SubGradient(percentage=43, r=0xAA, g=0x6C, b=0xFC, opacity=0.6),
-            SubGradient(percentage=63, r=0xD2, g=0x78, b=0xFC, opacity=0.3), 
+            SubGradient(percentage=63, r=0xD2, g=0x78, b=0xFC, opacity=0.3),
             SubGradient(percentage=75, r=0xEE, g=0xAC, b=0xFF, opacity=0.3),
-            SubGradient(percentage=100, r=0xFD, g=0xFD, b=0xFF,opacity=1.0),
+            SubGradient(percentage=100, r=0xFD, g=0xFD, b=0xFF, opacity=1.0),
         ],
     )
     s3_im = get_s3_image(data["image_id"])
-    base.paste(s3_im, (int(base.width/2),int(base.height/2)))
+    add_corners(s3_im, 100)
+    base.paste(s3_im, (int(base.width / 2 - base.width/4), int(base.height / 2 - base.height /4)), s3_im)
     font = ImageFont.load_default(size=42)
     pilmoji = Pilmoji(base)
-    pilmoji.text((512,512), f"I completed {data["achievement_name"]}! 😎", fill=(255,0,0), font=font)
+    pilmoji.text(
+        (256, 210),
+        f"I completed {data["achievement_name"]}! 😎",
+        fill=(0xEE, 0xAC, 0xFF),
+        font=font,
+    )
     base.show()
-    #s.paste(base, (0,0), base)
+    #s.paste((255, 255, 255), (0, 0, s.width, s.height))
+    #s.paste(base, (0, 0), base)
     #s.show()
-    #im.show()
+
+
+# https://stackoverflow.com/a/11291419
+def add_corners(im, rad):
+    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
+    alpha = Image.new('L', im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    im.putalpha(alpha)
+    return im
 
 def get_s3_image(image_id: str) -> Image:
     img_response = download(image_id)
     stream = io.BytesIO(img_response.content)
-    return Image.open(stream).resize((512,512))
+    return Image.open(stream).resize((512, 512))
+
 
 def draw_gradient(im: Image, stops: list[SubGradient]):
-    fill = (stops[0].r, stops[0].g, stops[0].b)
+    old = (stops[0].r, stops[0].g, stops[0].b)
     start = 0
 
     for g in stops:
-        end = int((g.percentage/100) * im.height)
-        fill = draw_subgradient(im=im, c1=fill, c2=(g.r, g.g, g.b), start=start, end=end, opacity=g.opacity)
+        end = int((g.percentage / 100) * im.height)
+        draw_subgradient(
+            im=im, c1=old, c2=(g.r, g.g, g.b), start=start, end=end, opacity=g.opacity
+        )
         start = end
+        old = (g.r, g.g, g.b)
+
 
 def rgb_tuple(hex_string: str) -> tuple[int, int, int]:
     r = int(hex_string[0:2], 16)
     g = int(hex_string[2:4], 16)
     b = int(hex_string[4:6], 16)
-    return (r,g,b)
+    return (r, g, b)
+
 
 # Draws background gradient from color 1 -> color 2
 # TODO: create new image for each subgradient to apply opacity
-def draw_subgradient(im: Image, c1: tuple[int, int, int], c2: tuple[int, int, int], start : int, end: int, opacity: float):
+def draw_subgradient(
+    im: Image,
+    c1: tuple[int, int, int],
+    c2: tuple[int, int, int],
+    start: int,
+    end: int,
+    opacity: float,
+):
     draw = ImageDraw.Draw(im)
     fill = c1
 
-    c_freq_r = change_freq(c1[0], c2[0], end-start)
-    c_freq_g = change_freq(c1[1], c2[1], end-start)
-    c_freq_b = change_freq(c1[2], c2[2], end-start)
+    c_freq_r = change_freq(c1[0], c2[0], end - start)
+    c_freq_g = change_freq(c1[1], c2[1], end - start)
+    c_freq_b = change_freq(c1[2], c2[2], end - start)
 
     increment_r = c1[0] < c2[0]
     increment_g = c1[1] < c2[1]
@@ -92,22 +129,24 @@ def draw_subgradient(im: Image, c1: tuple[int, int, int], c2: tuple[int, int, in
         if divides(x, c_freq_b) and not stop_b:
             b = new_color(b, increment_b, 0)
 
-        fill = (r,g,b)
-        
-        draw.line([(0,x), (im.width, x)], fill=fill, width=0)
-    return fill
+        fill = (r, g, b)
+
+        draw.line([(0, x), (im.width, x)], fill=fill, width=0)
+
 
 def divides(a: int, b: int):
     if a == 0:
         return False
     return a % b == 0
 
+
 # Change frequency determines how many iterations between 0 and the images height it will take before an intensity of a color is changed
 # It returns an int which means the value is going to be rounded, but it works well enough.
 def change_freq(a: int, b: int, height: int) -> int:
     if a == b:
         return 11
-    return int(height/abs(a-b))
+    return int(height / abs(a - b))
+
 
 def new_color(color: int, up: bool, index: int) -> int:
     if up:
@@ -116,10 +155,12 @@ def new_color(color: int, up: bool, index: int) -> int:
         color -= 1
     return color
 
+
 def has_reached_end(color1: int, color2, up: bool):
     if up:
         return color1 >= color2
     else:
         return color1 <= color2
 
-generate_image(achievement_name="Testtitel", id = 1234, image_id="171576751164")
+
+generate_image(achievement_name="Testtitel", id=1234, image_id="171576751164")
