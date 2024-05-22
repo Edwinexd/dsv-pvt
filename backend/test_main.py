bd39637413dc441d5bfc108b02480042290628d8
Alfred Berggren <alfred.berggren@gmail.com>
2024-05-22 08:51:14 +0200

    more tests

--- backend/test_main.py
+++ backend/test_main.py
@@ -91,8 +91,52 @@ def test_none_fields(client, mocker):
     assert response.status_code == 422
     assert "id" not in response.json()
 
-#TODO invalid email format (400)
-#TODO alredy existing email (400)
+def test_invalid_email_format(client, mocker):
+    user_payload = {
+        "email": "@examp@le.com@",
+        "username": "testuser4",
+        "password": "test123",
+        "full_name": "Test User",
+    }
+    mock_user_id = "user423"
+    mocker.patch("main.auth.create_user", return_value=mock_user_id)
+
+    response = client.post("/users", json=user_payload)
+
+    assert response.status_code == 400
+    assert "id" not in response.json()
+
+
+def test_already_existing_email(client, mocker): # 5
+    user_payload = {
+        "email": "test5@example.com",
+        "username": "testuser5",
+        "password": "test123",
+        "full_name": "Test User",
+    }
+    mock_user_id = "user523"
+    mocker.patch("main.auth.create_user", return_value=mock_user_id)
+
+    response = client.post("/users", json=user_payload)
+
+    assert response.status_code == 200
+    assert "id" in response.json()
+
+    user_payload2 = {
+        "email": "test5@example.com",
+        "username": "testuser52",
+        "password": "test1232",
+        "full_name": "Test User2",
+    }
+    mock_user_id2 = "user5232"
+    mocker.patch("main.auth.create_user", return_value=mock_user_id2)
+
+    response2 = client.post("/users", json=user_payload2) # Send same email again
+
+    assert response.status_code == 409
+    assert "Email already exists" in response.json()["detail"]
+
+
 #TODO nonexistent fields (422)
 #TODO: invalid input types (422)
 #TODO: test with wrong request method (put,delete) (405)
