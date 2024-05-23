49aae9cb7665f47a4fc8ef513aeb690b2128248b
Johan Berg <119810021+Johan-Berg-Engstrand@users.noreply.github.com>
2024-05-23 19:13:48 +0200

    Refactored group model since group skill level doesn't exists in backend anymore

--- flutter_application/lib/models/group.dart
+++ flutter_application/lib/models/group.dart
@@ -4,7 +4,7 @@ class Group {
   final String description;
   final bool isPrivate;
   final String ownerId;
-  final int skillLevel;
+  // final int skillLevel;
   final int points;
   final String? imageId;
   final String? address;
@@ -18,7 +18,7 @@ class Group {
       required this.description,
       required this.isPrivate,
       required this.ownerId,
-      required this.skillLevel,
+      // required this.skillLevel,
       this.points = 0,
       this.imageId,
       this.address,
@@ -33,7 +33,7 @@ class Group {
       description: json["description"] as String,
       isPrivate: json['is_private'] as bool,
       ownerId: json['owner_id'] as String,
-      skillLevel: json['skill_level'] as int,
+      // skillLevel: json['skill_level'] as int,
       points: json['points'] as int,
       imageId: json['image_id'] as String?,
       address: json['address'] as String?,
