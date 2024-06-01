aa23fb6f965bee35112c41c3265c55f336315e47
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-06-01 20:24:47 +0200

    Add license notice to all files (#130)

--- flutter_application/lib/launch_injector.dart
+++ flutter_application/lib/launch_injector.dart
@@ -1,11 +1,24 @@
+/*
+Lace Up & Lead The Way - A pre-race training app and social platform for runners.
+Copyright (C) 2024 Group 71 (PVT 7.5) Stockholm University
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 import 'package:flutter_application/controllers/backend_service.dart';
 import 'package:flutter/material.dart';
 import 'main.dart';
 import 'package:flutter_application/views/login_page.dart';
-
-// Page that fetches isLoggedIn from BackendService
-// if true, shows MainPage, else LoginPage
-// while fetching show a static image
 
 class LaunchInjector extends StatelessWidget {
   final bool darkModeEnabled;
